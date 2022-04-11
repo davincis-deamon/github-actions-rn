22bf73a4cb3e78a8a4ac77b51ed28a9fe857cbfe
Russel Rajitha <russel@solomoit.com>
2022-04-11 22:58:53 +0530

    Updated certificates

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -2,11 +2,11 @@
 set -eo pipefail
 
 gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
-#gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Github_Actions_Provisioning.mobileprovision ./.github/secrets/Github_Actions_Provisioning.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Github_Actions_Provisioning.mobileprovision ./.github/secrets/Github_Actions_Provisioning.mobileprovision.gpg
 #
-#mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 #
-#cp ./.github/secrets/Github_Actions_Provisioning.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Github_Actions_Provisioning.mobileprovision
+cp ./.github/secrets/Github_Actions_Provisioning.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Github_Actions_Provisioning.mobileprovision
 #
 #security create-keychain -p "" build.keychain
 #security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$APPLE_DISTRIBUTION_PASSWORD" -A
