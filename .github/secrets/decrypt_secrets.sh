c935c8f22a10596762b8bd7d3074e5903c22dc55
Russel Rajitha <russel@solomoit.com>
2022-04-11 22:35:57 +0530

    Updated certificates

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,26 +1,18 @@
 #!/bin/sh
-# create variables
-CERTIFICATE_PATH=$PWD/build_certificate.p12
-PP_PATH=$PWD/build_pp.mobileprovision
-KEYCHAIN_PATH=$PWD/app-signing.keychain-db
+set -eo pipefail
 
-# import certificate and provisioning profile from secrets
-echo -n "$APPLE_PROVISION_PROFILE_BASE64" | base64 --decode --output "$CERTIFICATE_PATH"
-echo -n "$BUILD_PROVISION_PROFILE_BASE64" | base64 --decode --output "$PP_PATH"
+gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Github_Actions_Provisioning.mobileprovision ./.github/secrets/Github_Actions_Provisioning.mobileprovision.gpg
 
-# create temporary keychain
-security create-keychain -p "$APPLE_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
-security set-keychain-settings -lut 21600 "$KEYCHAIN_PATH"
-security unlock-keychain -p "$APPLE_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
-
-# import certificate to keychain
-security import "$CERTIFICATE_PATH" -P "$APPLE_DISTRIBUTION_PASSWORD" -A -t cert -f pkcs12 -k "$KEYCHAIN_PATH"
-security list-keychain -d user -s "$KEYCHAIN_PATH"
-
-# apply provisioning profile
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "$PP_PATH" ~/Library/MobileDevice/Provisioning\ Profiles
-echo "RUNNER_TEMP : $RUNNER_TEMP"
-echo "PWD : $PWD"
-echo "Keychains updated"
 
+cp ./.github/secrets/Github_Actions_Provisioning.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Github_Actions_Provisioning.mobileprovision
+
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$APPLE_DISTRIBUTION_PASSWORD" -A
+
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
