0befbc2b6fe9d23c01c519f98e3a070ac29c8b34
Russel Rajitha <russel@solomoit.com>
2022-04-11 14:42:54 +0530

    Updated certificates

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -5,16 +5,12 @@ gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" 
 gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-
 cp ./.github/secrets/Github_Actions_Provisioning.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Github_Actions_Provisioning.mobileprovision
-
-
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
-
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
-
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+echo "Keychains updated"
 
