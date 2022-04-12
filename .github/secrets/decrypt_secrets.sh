9bd86303becde5f4a20078fd4e6b001e0c8ac0e0
Russel Rajitha <russel@solomoit.com>
2022-04-12 09:00:59 +0530

    Updated certificates

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -3,11 +3,13 @@ set -eo pipefail
 
 gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Github_Actions_Provisioning.mobileprovision ./.github/secrets/Github_Actions_Provisioning.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Certificates_developer.p12 ./.github/secrets/Certificates_developer.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/Github_Actions_Provisioning.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Github_Actions_Provisioning.mobileprovision
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$APPLE_DISTRIBUTION_PASSWORD" -A
+security import ./.github/secrets/Certificates_developer.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$APPLE_DEVELOPMENT_PASSWORD" -A
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
