900470633f02290f0887a94a8d4294df0dabf5fe
Russel Rajitha <russel@solomoit.com>
2022-04-12 10:19:22 +0530

    Updated certificates

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -4,9 +4,11 @@ set -eo pipefail
 gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Github_Actions_Provisioning.mobileprovision ./.github/secrets/Github_Actions_Provisioning.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Certificates_developer.p12 ./.github/secrets/Certificates_developer.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$APPLE_ENCRYPTION_PASSPHRASE" --output ./.github/secrets/Apple_Distribution_with_github.mobileprovision ./.github/secrets/Apple_Distribution_with_github.mobileprovision.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/Github_Actions_Provisioning.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Github_Actions_Provisioning.mobileprovision
+cp ./.github/secrets/Apple_Distribution_with_github.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Apple_Distribution_with_github.mobileprovision.gpg
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$APPLE_DISTRIBUTION_PASSWORD" -A
 security import ./.github/secrets/Certificates_developer.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$APPLE_DEVELOPMENT_PASSWORD" -A
