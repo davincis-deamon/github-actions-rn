9250698a83e21a2b4c5d850d02c75351d18e56ec
Russel Rajitha <russel@solomoit.com>
2022-04-13 09:08:52 +0530

    Updated certificates

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -2,10 +2,12 @@
 
 set -eo pipefail
 cd ios
-xcodebuild -workspace github.xcworkspace \
-            -scheme github \
-            -sdk iphoneos \
-            -configuration AppStoreDistribution \
-            -archivePath $PWD/build/github.xcarchive \
-            clean archive | xcpretty
+#xcodebuild -workspace github.xcworkspace \
+#            -scheme github \
+#            -sdk iphoneos \
+#            -configuration AppStoreDistribution \
+#            -archivePath $PWD/build/github.xcarchive \
+#            clean archive | xcpretty
+
+xcodebuild -exportArchive -exportOptionsPlist "github/exportOptions.plist" -archivePath "$PWD/build/github.xcarchive" -exportPath "$PWD/build/github.ipa"
 
