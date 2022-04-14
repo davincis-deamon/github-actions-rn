1736ec590115fcb75aa11fd1fa347fdc231a87a0
Russel Rajitha <russel@solomoit.com>
2022-04-14 08:37:52 +0530

    Updated certificates

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -12,4 +12,5 @@ security import ./.github/secrets/ios_distribution.p12 -t agg -k ~/Library/Keych
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
