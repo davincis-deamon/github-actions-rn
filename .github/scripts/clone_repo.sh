b3ca9051a056f56f4cf9ef3743046c167887ea6b
Russel Rajitha <russel@solomoit.com>
2022-04-24 20:19:15 +0530

    Added Secret decrypt

--- .github/scripts/clone_repo.sh
+++ .github/scripts/clone_repo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eo pipefail
 
-GIT_SSH_COMMAND='ssh -i ./.github/secrets/ssh_key -o IdentitiesOnly=yes' git clone "$PRIVATE_REPO_SSH_URL"
+GIT_SSH_COMMAND='ssh -i ./.github/secrets/ssh_key -o IdentitiesOnly=yes' git clone "$PRIVATE_REPO_SSH_URL" private_repo
 
--- .github/scripts/install_dependancies.sh
+++ .github/scripts/install_dependancies.sh
@@ -2,8 +2,9 @@
 
 set -eo pipefail
 echo $PWD
+cd "$PWD/private_repo"
 yarn install
 cd ios
 pod install --repo-update
- cd ..
+cd ..
 
