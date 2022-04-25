99ccf8a7c93707e87c316c730d489b1e15f8151b
Russel Rajitha <russel@solomoit.com>
2022-04-25 19:20:06 +0530

    Added Secret decrypt

--- .github/scripts/clone_repo.sh
+++ .github/scripts/clone_repo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eo pipefail
 
-GIT_SSH_COMMAND='ssh -i ./.github/secrets/ssh_key -o IdentitiesOnly=yes' git clone "$PRIVATE_REPO_1_SSH_URL" private_repo
+GIT_SSH_COMMAND='ssh -i ./.github/secrets/ssh_key -o IdentitiesOnly=yes' git clone --branch "$PRIVATE_REPO_BRANCH" "$PRIVATE_REPO_SSH_URL" private_repo
 
