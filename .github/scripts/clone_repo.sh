0671046ad478423bb67450ca0d4045ef0062f4d5
Russel Rajitha <russel@solomoit.com>
2022-05-11 09:00:09 +0530

    Initial Build

--- .github/scripts/clone_repo.sh
+++ .github/scripts/clone_repo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eo pipefail
-ssh-keygen -R "$REPO_5_HOSTNAME"
+ssh-keygen -f /Users/runner/.ssh/known_hosts -R "$REPO_5_HOSTNAME"
 GIT_SSH_COMMAND='ssh -i ./.github/secrets/ssh_key -o IdentitiesOnly=yes StrictHostKeyChecking=no -o StrictHostKeyChecking=no' git clone --branch "$PRIVATE_REPO_BRANCH" "$PRIVATE_REPO_SSH_URL" private_repo
 
