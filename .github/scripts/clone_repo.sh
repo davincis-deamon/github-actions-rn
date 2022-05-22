93901771390f572e69af7b702ae17ff2d32c6d12
Russel Rajitha <russel@solomoit.com>
2022-05-22 09:00:24 +0530

    Added Automation testing

--- .github/scripts/clone_repo.sh
+++ .github/scripts/clone_repo.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -eo pipefail
 GIT_SSH_COMMAND='ssh -i ./.github/secrets/ssh_key -o StrictHostKeyChecking=no' git clone --branch "$PRIVATE_REPO_BRANCH" "$PRIVATE_REPO_SSH_URL" private_repo
+cd private_repo
+git status
